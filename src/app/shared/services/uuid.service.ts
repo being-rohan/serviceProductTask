7680acedc831baece32620b94c846f58405678cf
rohan panchal <rohanpanchal216@gmail.com>
2024-02-28 10:38:43 +0530

    work on it

--- src/app/shared/services/uuid.service.ts
+++ src/app/shared/services/uuid.service.ts
@@ -6,13 +6,13 @@ import { Injectable } from '@angular/core';
 export class UuidService {
 
   constructor() { }
- generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
       const r = Math.random() * 16 | 0,
         v = c === 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
   }
-  
- 
+
+
 }
